ccb65d9d9398af68c84f431bc262be4587c5e43a
Chris Dalke <chrisdalke@gmail.com>
2019-03-17 13:23:59 -0400

    Removed release bundles; it's easier to just run the whole program from source

--- runRelease.sh
+++
@@ -1,9 +0,0 @@
-# Mounted bootloader script
-
-echo "Starting Telemetry Server..."
-
-# Make sure we're in the right directory
-cd /mnt/usb/
-
-# Run python telemetry server program
-release-bundles/release-1.0.0
