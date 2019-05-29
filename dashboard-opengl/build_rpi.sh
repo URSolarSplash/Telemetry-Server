edd6940f2095dcf2f10098640b83b83a2587c5c7
Chris Dalke <chrisdalke@gmail.com>
2019-05-29 10:43:55 -0400

    Working on updating build setup

--- dashboard-opengl/build_rpi.sh
+++ dashboard-opengl/build_rpi.sh
@@ -1,2 +1,2 @@
-clang lib/libraylib_rpi.a dashboard.c -o dashboard_rpi
+gcc -o dashboard_rpi dashboard.c -O1 -s -Wall -std=c99 -D_DEFAULT_SOURCE -Wno-missing-braces -std=gnu99 -I. -I/home/pi/raylib/src -I/home/pi/raylib/src/external -I/opt/vc/include -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads -L. -L/home/pi/raylib/src -L/opt/vc/lib -lraylib -lbrcmGLESv2 -lpthread -lrt -lm -lbcm_host -ldl -DPLATFORM_RPI
 ./dashboard_rpi
