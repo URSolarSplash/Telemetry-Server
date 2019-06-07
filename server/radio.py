d9593e117cedaa2214b7d5e63d8cc0100b0beb08
Chris Dalke <chrisdalke@gmail.com>
2019-06-07 13:32:11 -0400

    fixing variable names and adding debug printing

--- server/radio.py
+++ server/radio.py
@@ -4,6 +4,7 @@ import struct
 import time
 from server import config
 import binascii
+import traceback
 
 # Handles radio communication of telemetry data
 # This handles output from the Pi to the shore computer
@@ -13,7 +14,7 @@ import binascii
 class RadioManager():
     def __init__(self, dataCache):
         self.mode = 0
-        self.dataCache = dataCache
+        self.cache = dataCache
         self.lastUpdate = time.time()
         self.buffer = []
         try:
@@ -48,6 +49,7 @@ class RadioManager():
                 self.buffer = self.buffer[6:]
                 self.read(packet)
         except Exception as e:
+            traceback.print_exc()
             pass
 
         # Sends packets at a fixed rate
@@ -55,8 +57,8 @@ class RadioManager():
             return
         else:
             self.lastUpdate = time.time()
-        for i, key in enumerate(self.dataCache.getKeys()):
-            self.write(key, self.dataCache.get(key))
+        for i, key in enumerate(self.cache.getKeys()):
+            self.write(key, self.cache.get(key))
     def write(self,dataName, dataValue):
         # Writes a data point update to the radio stream if radio is active
         if self.mode == 1:
@@ -66,7 +68,7 @@ class RadioManager():
             # byte 3 - 6: data
             packet = bytearray(6)
             packet[0] = 0xF0
-            packet[1] = self.dataCache.keyToIndex(dataName)
+            packet[1] = self.cache.keyToIndex(dataName)
             if not dataValue is None:
                 packet[2:6] = bytearray(struct.pack(">f", dataValue))
             else:
