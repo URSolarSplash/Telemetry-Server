290d500811554a9c8e141c700c1e38d3722a5e0e
Leon <leon@leonzong.com>
2025-03-28 14:03:44 -0400

    update dashboard build

--- dashboard-opengl/build_rpi.sh
+++
@@ -1,2 +0,0 @@
-gcc -o dashboard_rpi dashboard.c json.c -O1 -s -Wall -std=c99 -D_DEFAULT_SOURCE -Wno-missing-braces -std=gnu99 -I. -I/home/pi/raylib/src -I/home/pi/raylib/src/external -I/opt/vc/include -I/opt/vc/include/interface/vmcs_host/linux -I/opt/vc/include/interface/vcos/pthreads -L. -L./lib -L/home/pi/raylib/src -L/opt/vc/lib -lraylib -lrequests_rpi -lbrcmGLESv2 -lbrcmEGL -lpthread -lrt -lm -lbcm_host -ldl -lcurl -DPLATFORM_RPI
-./dashboard_rpi
